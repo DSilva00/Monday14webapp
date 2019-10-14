b6f28c190d3eeb99f3a566c9bf480a1f8dac4c49
Diogo Santos Silva <21814203@bucks.ac.uk>
2019-10-14 11:42:18 +0100

    Added CRUD pages dor Students

---
+++ WebApplication1/Data/UniversityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext (DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Student> Student { get; set; }
+    }
+}
